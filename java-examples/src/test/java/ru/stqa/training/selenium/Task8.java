f476843012ecf3f232773e4a569fc34e5b3cc76e
julja83 <59063644+julja83@users.noreply.github.com>
2020-09-09 15:28:44 +0300

    Задание 8

--- java-examples/src/test/java/ru/stqa/training/selenium/Task8.java
+++ java-examples/src/test/java/ru/stqa/training/selenium/Task8.java
@@ -1,13 +1,13 @@
 package ru.stqa.training.selenium;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,34 +33,15 @@ public class Task8 {
 
     new WebDriverWait(driver, 10)
         .until(ExpectedConditions.visibilityOfAllElements(cards));
-
-   /* for (int i = 1; i <= mainMenu.size(); i++) {
-      driver.findElement(By.xpath("(//ul[@id='box-apps-menu']//li[@id='app-'])[" + i + "]"))
-          .click();
-      wait.until(presenceOfElementLocated(By.xpath("//h1")));
-      //если есть вложенное меню
-      if (isElementPresent(
-          By.xpath("(//ul[@id='box-apps-menu']//li[@id='app-'])[" + i + "]//li"))) {
-
-        List<WebElement> insertedMenu = driver
-            .findElements(By.xpath("(//ul[@id='box-apps-menu']//li[@id='app-'])[" + i + "]//li"));
-
-        for (int j = 1; j <= insertedMenu.size(); j++) {
-          driver.findElement(
-              By.xpath("(//ul[@id='box-apps-menu']//li[@id='app-'])[" + i + "]//li[" + j + "]"))
-              .click();
-          wait.until(presenceOfElementLocated(By.xpath("//h1")));
-        }
-      }
-    }*/
-  }
-
-  public boolean isElementPresent(By locator) {
-    try {
-      driver.findElement(locator);
-      return true;
-    } catch (NoSuchElementException ex) {
-      return false;
+    List<WebElement> newSticker = driver.findElements(By.xpath("//div[@class='sticker new']"));
+    List<WebElement> saleSticker = driver.findElements(By.xpath("//div[@class='sticker sale']"));
+    //стикеров столько же сколько товара
+    assertThat(cards.size(), equalTo(newSticker.size() + saleSticker.size()));
+    //каждый товар имеет один стикер (одной этой проверки достаточно для Задания 8)
+    for (int i = 1; i < cards.size(); i++) {
+      List<WebElement> stickers = driver.findElements(
+          By.xpath("(//li//a[@class='link'])[" + i + "]//div[contains(@class,'sticker')]"));
+      assertThat(stickers.size(), equalTo(1));
     }
   }
 
